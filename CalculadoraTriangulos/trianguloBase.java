ef7e235d866fe41d0e436a99d971646f897f9352
Gustavo Hernandez <162768131+Guzmonkey@users.noreply.github.com>
2024-09-13 09:18:40 -0700

    Update trianguloBase.java

--- CalculadoraTriangulos/trianguloBase.java
+++ CalculadoraTriangulos/trianguloBase.java
@@ -1,4 +1,5 @@
 public class trianguloBase {
+    // Declaración de atributos
     private double ladoA;
     private double ladoB;
     private double ladoC;
@@ -6,9 +7,14 @@ public class trianguloBase {
     private double anguloB;
     private double anguloC;
     
-    // Constructor vacío 
+    /*
+     * Constructor vacío
+     */
     public trianguloBase(){}
 
+    /*
+     * Constructor de la clase trianguloBase
+     */
     public trianguloBase(double ladoA, double ladoB, double ladoC, double anguloA, double anguloB, double anguloC) {
         this.ladoA = ladoA;
         this.ladoB = ladoB;
@@ -18,24 +24,37 @@ public class trianguloBase {
         this.anguloC = anguloC;
     }
 
+    /*
+     * Método que valida si se puede formar un triangulo con 3 lados
+     */
     public boolean validarTrianguloCon3Lados(double ladoA, double ladoB, Double ladoC){
+        // Comparamos la suma de dos lados tiene que ser mayor al lado restante
         if(ladoA + ladoB > ladoC && ladoA + ladoC > ladoB && ladoB + ladoC > ladoA){
+            // Retorna verdadero si se cumple la condición
             return true;
         }else{
+            // Retorna falso si no se cumple la condición
             return false;
         }
     }
 
+    /*
+     * Método que valida si se ingresan angulos permitidos
+     */
     public boolean validarTrianguloCon3Angulos(double anguloA, double anguloB, double anguloC){
-        if(anguloA <= -1|| anguloB <= -1 || anguloC <= -1 ){
-            return false;
+        if(anguloA <= -1|| anguloB <= -1 || anguloC <= -1 ){ // Comparamos si se ingresa un número menor o igual a -1
+            return false; // Retorna false
+        // Comparamos si se ingresan angulos igual o mayores a 180
         }else if(anguloA >= 180 || anguloB >= 180 || anguloC >= 180){
-            return false;
-        }else{
+            return false; // Retornamos false
+        }else{ // Si se cumple la condición de agregar números permitidos regresa true
             return true;
         }
     }
 
+    /*
+     * Métodos Getters y Setters de la clase trianguloBase
+     */
     public double getLadoA() {
         return ladoA;
     }
@@ -84,6 +103,10 @@ public class trianguloBase {
         this.anguloC = anguloC;
   
     }
+    
+    /*
+     * Método toString
+     */
     @Override
     public String toString() {
         return "Triangulo " + "ladoA=" + ladoA + ", ladoB=" + ladoB + ", ladoC=" + ladoC + ", anguloA=" + anguloA + ", anguloB=" + anguloB + ", anguloC=" + anguloC;
